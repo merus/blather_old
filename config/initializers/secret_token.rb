511ea55456a4a179e976bf51d7481a27552336e3
Matt Cramp <mattcrampy@gmail.com>
2011-07-30 17:59:34 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blather::Application.config.secret_token = 'ca5dfb99e2db4eb227c4d349d59f507ecfa3180786037a0878618b574f07840abe5066e5cedd87ac6adbe2757a3d19671d222100662fe8038675d7bf46632c3a'
