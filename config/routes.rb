5a82e12000e44a1035b1e0118c7cc25bf1648a5c
Matt Cramp <mattcrampy@gmail.com>
2011-08-01 01:54:28 +1000

    Layout and routes done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Blather::Application.routes.draw do
+  get "users/new"
+
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
