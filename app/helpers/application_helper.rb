a2899c76092d22e6047c0fddfdf3c4d11f34e7b9
Matt Cramp <mattcrampy@gmail.com>
2011-08-01 00:20:24 +1000

    Removed default Rails page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  #Return a specific title if one exists, and a default title otherwise
+  def title
+    base_title = "Blather"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
+
 end
