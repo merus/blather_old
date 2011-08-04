8543c1cd8d5ff4202e36366dbf46cef7a7bf383c
Matt Cramp <mattcrampy@gmail.com>
2011-08-04 18:58:48 +1000

    Finished first cut of the user model

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 module ApplicationHelper
+  #Return the logo location
+
+  def logo
+    logo = image_tag("logo.png", :alt => "Sample App", :class => "round")
+  end
 
   #Return a specific title if one exists, and a default title otherwise
   def title
